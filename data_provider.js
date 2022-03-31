391b36b243a8ed858431440abb6b297470e43f0a
Sylvain Gournay <sylvain.gournay@prismea.fr>
2022-03-31 17:50:10 +0200

    read jobs from file + refacto

--- data_provider.js
+++ data_provider.js
@@ -9,15 +9,41 @@ const csv_to_profession = (line) => {
   }
 }
 
+const csv_to_jobs = (line) => {
+  const attributes = line.split(',');
+  return {
+    profession_id: Number(attributes[0]),
+    contract_type: attributes[1],
+    name: attributes[2],
+    office_latitude: attributes[3],
+    office_longitude: attributes[4],
+  }
+}
+
+const read_file_with_header = (file, line_mapper) => {
+  return readFile(file, 'utf8')
+    .then(data => {
+      return data.split('\n')
+        .slice(1)
+        .map(line_mapper);
+    });
+}
+
 const read_professions = () => {
-  return readFile('./data/technical-test-professions.csv', 'utf8')
-      .then(data => {
-        return data.split('\n')
-            .slice(1)
-            .map(csv_to_profession);
-      });
+  return read_file_with_header(
+    './data/technical-test-professions.csv',
+    csv_to_profession
+  );
+}
+
+const read_jobs = () => {
+  return read_file_with_header(
+    './data/technical-test-jobs.csv',
+    csv_to_jobs
+  );
 }
 
 module.exports = {
   read_professions,
+  read_jobs,
 };
--- data_provider.test.js
+++ data_provider.test.js
@@ -1,6 +1,6 @@
 const data_provider = require('./data_provider');
 
-test('Should retrieve at least professions', async () => {
+test('Should retrieve at least one profession', async () => {
   const result = await data_provider.read_professions();
 
   expect(result).toBeDefined();
@@ -10,6 +10,22 @@ test('Should retrieve at least professions', async () => {
       id: 17,
       name: 'Devops / Infrastructure',
       category_name: 'Tech',
+    },
+  );
+});
+
+test('Should retrieve at least one job', async () => {
+  const result = await data_provider.read_jobs();
+
+  expect(result).toBeDefined();
+  expect(result.length).toBeGreaterThan(0);
+  expect(result[0]).toMatchObject(
+    {
+      profession_id: 7,
+      contract_type: 'INTERNSHIP',
+      name: '[Louis Vuitton Germany] Praktikant (m/w) im Bereich Digital Retail (E-Commerce)',
+      office_latitude: '48.1392154',
+      office_longitude: '11.5781413',
     },
   );
 });
